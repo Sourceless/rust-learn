9997fbe36e93b30b1df9aafe727e59aabb68f522
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-30 01:00:50 +0100

    Add remainder of section 3

---
+++ 3_Syntax_Basics/operators.rs
@@ -0,0 +1,3 @@
+let x: float = 4.0;
+let y: uint = x as uint;
+assert!(y == 4u);
---
+++ 3_Syntax_Basics/syntax_extensions.rs
@@ -0,0 +1,4 @@
+io::println(fmt!("%s is %d", "the answer", 43));
+
+// %? will conveniently print any type
+io::println(fmt!("what is this thing: %?.", mystery_object));
